3408abba3d2a386402e8207afb3ed76dc797f820
Tsvetan Dimoff <tsdimo@gmail.com>
2024-11-13 19:45:41 +0200

    Add deployment config

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,12 +11,10 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      {/* Overlay */}
       <div
         className="absolute inset-0 bg-black opacity-50"
         onClick={onClose}
       ></div>
-      {/* Modal Content */}
       <div className="bg-white rounded-lg p-6 z-60 max-w-md w-full relative">
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
